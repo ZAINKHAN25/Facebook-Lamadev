350c9902ac345eac54af903c5fa8991ed59992d3
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-10-17 11:00:08 -0700

    kam chal rha hai

--- src/components/pagenotfound/pagenotfound.jsx
+++ src/components/pagenotfound/pagenotfound.jsx
@@ -6,7 +6,7 @@ export default function Pagenotfound() {
             <div className="pagenotfountwrapper">
                 <h1 className="headingone">404</h1>
                 <h3 className="headingthree">Not Found</h3>
-                <p className="para">The resource requested could not be found on this server!</p>
+                <p className="para">The resource requested could not be found on this site!</p>
             </div>
         </div>
     )
